a650f769edf5f9ce94ea0165d35f6db3a09480c7
karenjin <karenajin@gmail.com>
2016-03-02 10:27:38 -0500

    Initial commit

---
+++ app/src/main/java/com/example/karenjin/criminalintent/CrimeActivity.java
@@ -0,0 +1,34 @@
+package com.example.karenjin.criminalintent;
+
+
+import android.app.FragmentTransaction;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentManager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+
+public class CrimeActivity extends FragmentActivity {
+
+    private final static String TAG = "CrimeActivity";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_crime);
+
+        FragmentManager fm = getSupportFragmentManager();
+        Fragment fragment = fm.findFragmentById(R.id.fragment_container);
+
+        if (fragment == null) {
+            fragment = new CrimeFragment();
+            fm.beginTransaction()
+                    .add(R.id.fragment_container, fragment)
+                    .commit();
+        }
+
+        Log.d(TAG, "inside oncreate()");
+
+    }
+}
